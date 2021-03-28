2598caa5d1eb3b158616bfca28ddfb160b5a5816
S-Sangha <72164204+S-Sangha@users.noreply.github.com>
2021-03-28 12:04:37 +0100

    Reverted the group study without the side bar

--- peer-learning/src/general/groupstudy.js
+++ peer-learning/src/general/groupstudy.js
@@ -1,14 +1,10 @@
 import {Link} from "react-router-dom";
 import React, { Component } from "react";
-import './sample.css';
-import NavBar from "./Navbar";
 
 export default class GroupStudy extends Component {
     render() {
         return (
-            <div>
-                <NavBar/>
-                <div id="mainPage">
+            <div className="Group">
               <a className="message">Want to get cracking?</a>
               <Link to="/newstudygroup">
               <button type="Create my own study group" className="btn btn-dark btn-lg btn-block">Create my own study group</button>
@@ -17,7 +13,6 @@ export default class GroupStudy extends Component {
               <button type="Join my study buddies" className="btn btn-dark btn-lg btn-block">Join my study buddies</button>
               </Link>
             </div>
-            </div>
         );
     }
-}
+} 
